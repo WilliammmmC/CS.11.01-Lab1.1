2877650b4c0413eb773c07e0e3cbfb30b4232099
WilliammmmC <157076893+WilliammmmC@users.noreply.github.com>
2024-02-02 09:12:44 +0800

    yay

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+        System.out.println(age);
+        System.out.println("minimum age to date:");
+        System.out.println(age/2 + 7);
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan2.nextInt();
+        System.out.println(age);
+        System.out.println("minimum page to read:");
+        System.out.println(100- age);
+
+
+
 
     }
 }
